2cc3801cb5e972cab39508a0ee4f86da087b219c
Miguel Angel Fernandez Diaz <mafernandez@stratio.com>
2015-07-16 10:09:51 +0200

    Important improvements

--- src/main/java/com/rojocarmesi/cassandra/data/filler/main.java
+++ src/main/java/com/rojocarmesi/cassandra/data/filler/main.java
@@ -6,28 +6,57 @@ import org.apache.commons.lang.RandomStringUtils;
 
 import java.util.*;
 
+import io.codearte.jfairy.Fairy;
+
 public class main {
 
     public static void main(String[] args) {
 
-        //DONE: boolean lowercase
+        //DONE: show keyspaces and tablenames
         //DONE: loop
         //DONE: sizes
-        //DONE: allow patterns
         //DONE: number ranges
         //DONE: allow sets
-        //TODO: bug
-        //TODO: requiring data according to type
-        //TODO: random with jfairy
-        //TODO: secuencial type when numeric
+        //DONE: allow patterns
+        //DONE: boolean lowercase
+        //DONE: requiring data according to type
+        //DONE: sequential type when numeric
+        //DONE: basic random data with jfairy
+        //TODO: advanced random data with jfairy
         //TODO: refactor
         //TODO: tests
-        //DONE: show keyspaces and tablenames
+
+        List<DataType> stringDataTypes = new ArrayList<>();
+        stringDataTypes.add(DataType.ascii());
+        stringDataTypes.add(DataType.text());
+        stringDataTypes.add(DataType.varchar());
+        stringDataTypes.add(DataType.blob());
+        stringDataTypes.add(DataType.inet());
+
+        List<DataType> uuidDataTypes = new ArrayList<>();
+        uuidDataTypes.add(DataType.timeuuid());
+        uuidDataTypes.add(DataType.uuid());
+
+        List<DataType> numericTypes = new ArrayList<>();
+        numericTypes.add(DataType.bigint());
+        numericTypes.add(DataType.cdouble());
+        numericTypes.add(DataType.cfloat());
+        numericTypes.add(DataType.cint());
+        numericTypes.add(DataType.counter());
+        numericTypes.add(DataType.decimal());
+        numericTypes.add(DataType.timestamp());
+        numericTypes.add(DataType.varint());
 
         Scanner scan = new Scanner(System.in);
 
+        Fairy fairy = Fairy.create();
+
+        System.out.println("=== CASSANDRA DATA FILLER ===");
+        System.out.print("Host: ");
+        String host = scan.nextLine();
+
         CassandraDriver cDriver = new CassandraDriver();
-        cDriver.connect("127.0.0.1");
+        cDriver.connect(host);
 
         boolean repeat;
 
@@ -35,7 +64,7 @@ public class main {
 
             // SHOW KEYSPACES
             List<KeyspaceMetadata> keyspaces = cDriver.getKeyspaces();
-            System.out.println("KEYSPACES: [");
+            System.out.print("KEYSPACES: [ ");
             for(KeyspaceMetadata keyspace: keyspaces){
                 if(!keyspace.getName().startsWith("system")) {
                     System.out.print(keyspace.getName() + " ");
@@ -49,7 +78,7 @@ public class main {
 
             // SHOW TABLENAMES
             Collection<TableMetadata> tablenames = cDriver.getTablenames(keyspace);
-            System.out.println("TABLENAMES: [");
+            System.out.print("TABLENAMES: [ ");
             for(TableMetadata tablename: tablenames){
                 System.out.print(tablename.getName()+" ");
             }
@@ -64,47 +93,85 @@ public class main {
             try {
                 desiredRows = Integer.parseInt(scan.nextLine());
             } catch (NumberFormatException nfe) {
-                System.err.println("3rd argument must be a positive number");
+                System.err.println("Number must be positive");
                 System.exit(-1);
             }
 
             TableMetadata tableMetadata = cDriver.getTableMetadata(keyspace, tablename);
 
-            Map<String, Integer[]> sizes = new HashMap<>();
-            Map<String, Boolean> lowercase = new HashMap<>();
+            Map<String, Integer[]> ranges = new HashMap<>();
+            Map<String, Boolean> sequential = new HashMap<>();
             Map<String, String[]> strings = new HashMap<>();
             Map<String, String> patterns = new HashMap<>();
+            Map<String, Boolean> lowercase = new HashMap<>();
 
-            for (ColumnMetadata columnMetadata : tableMetadata.getColumns()) {
+            for (ColumnMetadata columnMetadata: tableMetadata.getColumns()) {
                 // METADATA
                 String colName = columnMetadata.getName();
                 String colType = columnMetadata.getType().getName().name();
 
                 // SIZE
-                System.out.print("Size for " + colName + "(" + colType + ") [min - max]: ");
-                String[] rangeArray = scan.nextLine().trim().split("-");
-                sizes.put(colName, new Integer[]{Integer.parseInt(rangeArray[0].trim()), Integer.parseInt(rangeArray[1].trim())});
-
-                // GET SET OF STRINGS
-                System.out.print("Define set of words [str1, str2, ... strN]: ");
-                String strs = scan.nextLine();
-                if(strs.contains(",")) {
-                    strings.put(colName, strs.trim().split(","));
+                System.out.println(" - Data for " + colName + " (Type: " + colType + "): ");
+                if(numericTypes.contains(columnMetadata.getType())){
+                    System.out.print("Range [min - max] (Default: 0-" + (desiredRows-1) + "): ");
+                    String sizeInString = scan.nextLine().trim();
+                    if(sizeInString.isEmpty()){
+                        ranges.put(colName, new Integer[] {0, desiredRows-1});
+                    } else {
+                        String[] rangeArray = sizeInString.split("-");
+                        ranges.put(colName, new Integer[] {Integer.parseInt(rangeArray[0].trim()),
+                                Integer.parseInt(rangeArray[1].trim())});
+                    }
+                    System.out.print("Sequential? [Y/n] (Default: No): ");
+                    String isSequential = scan.nextLine();
+                    if(isSequential.trim().toLowerCase().startsWith("y")){
+                        sequential.put(colName, Boolean.TRUE);
+                    } else {
+                        sequential.put(colName, Boolean.FALSE);
+                    }
                 } else {
-                    strings.put(colName, new String[]{strs.trim()});
+                    ranges.put(colName, new Integer[] {0, desiredRows-1});
+                    sequential.put(colName, Boolean.FALSE);
                 }
 
-                // GET PATTERN
-                System.out.print("Define pattern [Example: *@*.com]: ");
-                String pattern = scan.nextLine();
-                patterns.put(colName, pattern);
+                if (stringDataTypes.contains(columnMetadata.getType())){
+                    // GET SET OF STRINGS
+                    String[] randomSet = new String[2];
+                    String city1 = fairy.person().getAddress().getCity();
+                    String city2 = fairy.person().getAddress().getCity();
+                    while(city2.equalsIgnoreCase(city1)){
+                        city2 = fairy.person().getAddress().getCity();
+                    }
+                    randomSet[0] = city1;
+                    randomSet[1] = city2;
 
-                // LOWERCASE
-                System.out.print("lowercase? [Y/n]: ");
-                String toLowercase = scan.nextLine();
-                if(toLowercase.trim().toLowerCase().startsWith("y")){
-                    lowercase.put(colName, Boolean.TRUE);
+                    System.out.print("Define set of words [str1, str2, ... strN] (Default: "
+                            + Arrays.toString(randomSet) + "): ");
+                    String strs = scan.nextLine();
+                    if(strs.isEmpty()){
+                        strings.put(colName, randomSet);
+                    } else if(strs.contains(",")) {
+                        strings.put(colName, strs.trim().split(","));
+                    } else {
+                        strings.put(colName, new String[]{strs.trim()});
+                    }
+
+                    // GET PATTERN
+                    System.out.print("Define pattern [Example: *@*.com] (Default: *): ");
+                    String pattern = scan.nextLine();
+                    patterns.put(colName, pattern);
+
+                    // LOWERCASE
+                    System.out.print("Lowercase? [Y/n] (Default: No): ");
+                    String toLowercase = scan.nextLine();
+                    if(toLowercase.trim().toLowerCase().startsWith("y")){
+                        lowercase.put(colName, Boolean.TRUE);
+                    } else {
+                        lowercase.put(colName, Boolean.FALSE);
+                    }
                 } else {
+                    strings.put(colName, new String[]{""});
+                    patterns.put(colName, "");
                     lowercase.put(colName, Boolean.FALSE);
                 }
             }
@@ -120,18 +187,9 @@ public class main {
                 sb.deleteCharAt(sb.length() - 1);
                 sb.deleteCharAt(sb.length() - 1);
                 sb.append(") VALUES (");
-                List<DataType> stringDataTypes = new ArrayList<>();
-                stringDataTypes.add(DataType.ascii());
-                stringDataTypes.add(DataType.text());
-                stringDataTypes.add(DataType.varchar());
-                stringDataTypes.add(DataType.blob());
-                stringDataTypes.add(DataType.inet());
-                List<DataType> uuidDataTypes = new ArrayList<>();
-                uuidDataTypes.add(DataType.timeuuid());
-                uuidDataTypes.add(DataType.uuid());
-                for (ColumnMetadata columnMetadata : tableMetadata.getColumns()) {
-                    Integer min_size = sizes.get(columnMetadata.getName())[0];
-                    Integer max_size = sizes.get(columnMetadata.getName())[1];
+                for (ColumnMetadata columnMetadata: tableMetadata.getColumns()) {
+                    Integer min_size = ranges.get(columnMetadata.getName())[0];
+                    Integer max_size = ranges.get(columnMetadata.getName())[1];
                     int range = max_size - min_size;
                     int randomInt = (int) (Math.random() * range)+1+min_size;
                     randomInt++;
@@ -177,8 +235,14 @@ public class main {
                         }
                     } else if (uuidDataTypes.contains(columnMetadata.getType())) {
                         sb.append("'").append(UUID.randomUUID().toString()).append("'");
+                    } else if (numericTypes.contains(columnMetadata.getType())){
+                        if(sequential.get(columnMetadata.getName())){
+                            sb.append(i+min_size);
+                        } else {
+                            sb.append(randomInt);
+                        }
                     } else {
-                        sb.append(randomInt);
+                        sb.append("''");
                     }
                     sb.append(", ");
                 }
